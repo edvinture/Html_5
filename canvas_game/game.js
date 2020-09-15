5c3f051ee408c51c35326270c8be9ae98ec16f63
IvanSerov <VanSer1989@gmail.com>
2020-09-15 12:33:08 +0300

    5 lvl rabotaet

--- canvas_game/game.js
+++ canvas_game/game.js
@@ -13,6 +13,19 @@ let game = {
     },
     init: function() {
         this.ctx = document.getElementById("mycanvas").getContext("2d");
+        this.setEvents();
+    },
+    setEvents() {
+        window.addEventListener("keydown", e => {
+            if (e.keyCode === 37) {
+                this.platform.dx = -this.platform.velocity;
+            } else if (e.keyCode === 39) {
+                this.platform.dx = this.platform.velocity;
+            }
+        });
+        window.addEventListener("keyup", e => {
+            this.platform.dx = 0;
+        });
     },
     preload(callback) {
         let loaded = 0;
@@ -21,37 +34,38 @@ let game = {
             ++loaded;
             if (loaded >= required) {
                 callback();
-        }
-    };
+            }
+        };
 
         for (let key in this.sprites) {
             this.sprites[key] = new Image();
             this.sprites[key].src = "img/" + key + ".png";
             this.sprites[key].addEventListener("load", onImageLoad);
-                
-                }
-            },
-            create() {
-                for (let row = 0; row < this.rows; row++) {
-                    for (let col = 0; col < this.cols; col++) {
-                        this.blocks.push({
-                            x: 64 * col + 65,
-                            y: 24 * row + 35
-                        });
-                    }
-                }
-            },
-        
-    
+        }
+    },
+    create() {
+        for (let row = 0; row < this.rows; row++) {
+            for (let col = 0; col < this.cols; col++) {
+                this.blocks.push({
+                    x: 64 * col + 65,
+                    y: 24 * row + 35
+                });
+            }
+        }
+    },
+    update() {
+        this.platform.move();
+    },
     run() {
         window.requestAnimationFrame(() => {
+            this.update();
             this.render();
+            this.run();
         });
     },
     render() {
         this.ctx.drawImage(this.sprites.background, 0, 0);
-        this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height,
-         this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+        this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x, this.ball.y, this.ball.width, this.ball.height);
         this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
         this.renderBlocks();
     },
@@ -77,13 +91,19 @@ game.ball = {
 };
 
 game.platform = {
+    velocity: 6,
+    dx: 0,
     x: 280,
-    y: 300
+    y: 300,
+    move() {
+        if (this.dx) {
+            this.x += this.dx;
+        }
+    }
 };
-
-
- 
 
 window.addEventListener("load", () => {
     game.start();
 });
+
+
